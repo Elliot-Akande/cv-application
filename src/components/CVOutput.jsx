9fb1f9fcc7eb34c392082b61d4ed950ee524a35d
Elliot Akande <elbo.lynch@gmail.com>
2023-10-29 10:41:55 +0000

    Add basic structure

--- src/components/CVOutput.jsx
+++ src/components/CVOutput.jsx
@@ -1,9 +1,57 @@
-function CVOutput(props) {
+function CVOutput({ personal, education, experience }) {
   return (
     <>
-      <h1>{props.name}</h1>
+      <header>
+        <h1>{personal.name}</h1>
+        <div>
+          <p>{personal.email}</p>
+          <p>{personal.phone}</p>
+          <p>{personal.address}</p>
+        </div>
+      </header>
+      <main>
+        {!!education && (
+          <section>
+            <h2>Education</h2>
+            {education.map((data) => (
+              <div key={data.id}>
+                <div>
+                  <p>
+                    {data.start} - {data.end}
+                  </p>
+                  <p>{data.location}</p>
+                </div>
+                <div>
+                  <h3>{data.school}</h3>
+                  <p>{data.degree}</p>
+                </div>
+              </div>
+            ))}
+          </section>
+        )}
+        {!!experience && (
+          <section>
+            <h2>Professional Experience</h2>
+            {experience.map((data) => (
+              <div key={data.id}>
+                <div>
+                  <p>
+                    {data.start} - {data.end}
+                  </p>
+                  <p>{data.location}</p>
+                </div>
+                <div>
+                  <h3>{data.company}</h3>
+                  <p>{data.position}</p>
+                  <p>{data.description}</p>
+                </div>
+              </div>
+            ))}
+          </section>
+        )}
+      </main>
     </>
-  )
+  );
 }
 
-export default CVOutput
+export default CVOutput;
