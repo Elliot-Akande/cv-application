0616064b51ea74108c20effc241290f72ad4c7d9
Elliot Akande <elbo.lynch@gmail.com>
2023-11-09 12:52:58 +0000

    Create EducationForm component

---
+++ src/components/EducationForm.jsx
@@ -0,0 +1,57 @@
+function EducationForm({ data, setData }) {
+  const { school, degree, start, end, location } = data;
+
+  return (
+    <form>
+      <ul>
+        <li>
+          <label htmlFor="school">School</label>
+          <input
+            type="text"
+            id="school"
+            value={school}
+            onChange={(e) => setData("school", e.target.value)}
+          />
+        </li>
+        <li>
+          <label htmlFor="degree">Degree</label>
+          <input
+            type="text"
+            id="degree"
+            value={degree}
+            onChange={(e) => setData("degree", e.target.value)}
+          />
+        </li>
+        <li>
+          <label htmlFor="start">Start</label>
+          <input
+            type="text"
+            id="start"
+            value={start}
+            onChange={(e) => setData("start", e.target.value)}
+          />
+        </li>
+        <li>
+          <label htmlFor="end">End</label>
+          <input
+            type="text"
+            id="end"
+            value={end}
+            onChange={(e) => setData("end", e.target.value)}
+          />
+        </li>
+        <li>
+          <label htmlFor="location">Location</label>
+          <input
+            type="text"
+            id="location"
+            value={location}
+            onChange={(e) => setData("location", e.target.value)}
+          />
+        </li>
+      </ul>
+    </form>
+  );
+}
+
+export default EducationForm;
