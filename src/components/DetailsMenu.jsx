eabf74f59037226293896b739e4529bc5bc433a0
Elliot Akande <elbo.lynch@gmail.com>
2023-11-10 11:23:49 +0000

    Import styles and Add icon

--- src/components/DetailsMenu.jsx
+++ src/components/DetailsMenu.jsx
@@ -1,11 +1,23 @@
-function DetailsMenu({ children, title, isOpen, handleMenuPressed }) {
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import "/src/styles/DetailsMenu.css";
+
+function DetailsMenu({ children, title, isOpen, handleMenuPressed, icon }) {
+  const Icon = icon;
+
   return (
-    <>
-      <button onClick={handleMenuPressed}>
-        <h2>{title}</h2>
+    <div className="detailsMenu__container">
+      <button
+        onClick={handleMenuPressed}
+        className={`detailsMenu ${!isOpen ? "" : "detailsMenu--active"}`}
+      >
+        <h2>
+          <Icon style={{ fontSize: "28px" }} />
+          {title}
+          <ExpandMoreIcon style={{ fontSize: "32px" }} />
+        </h2>
       </button>
       {!!isOpen && children}
-    </>
+    </div>
   );
 }
 
